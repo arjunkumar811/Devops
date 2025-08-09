589de508abd3aaa094cf3d2c1f2da84e4fd0e1c1
arjunkumar811 <kumararjun26401@gmail.com>
2025-08-09 19:51:20 +0530

    Added Code

--- Week 03 - CICD & Process management/Week 3.2 - Continuing CI, certificate management and testing/deployment/apps/web/app/page.tsx
+++ Week 03 - CICD & Process management/Week 3.2 - Continuing CI, certificate management and testing/deployment/apps/web/app/page.tsx
@@ -1,23 +1,3 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-import { client } from "@repo/db/client";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default function Home() {
   <div>
---
+++ Week 03 - CICD & Process management/Week 3.2 - Continuing CI, certificate management and testing/deployment/apps/ws-server/index.ts
@@ -0,0 +1,1 @@
+hi there iam a web soket server
